d6a8a9192a0dc3fb9c854b59bc274abf3dfc0d3c
J-888 <josemigueltajuelo@gmail.com>
2017-03-04 17:30:07 +0100

    Mayor visual changes to showcase view

--- consolescript.js
+++ consolescript.js
@@ -115,11 +115,13 @@ var Typer = {
 	},
 
 	updLstChr: function() { // blinking cursor
-		var cont = this.content(); // get console 
-		if (cont.substring(cont.length - 1, cont.length) == "|") // if last char is the cursor
-			$("#console").html($("#console").html().substring(0, cont.length - 1)); // remove it
-		else
-			this.write("|"); // else write it
+		if(this.file != "consoleContent/showcase.html") {
+			var cont = this.content(); // get console 
+			if (cont.substring(cont.length - 1, cont.length) == "|") // if last char is the cursor
+				$("#console").html($("#console").html().substring(0, cont.length - 1)); // remove it
+			else
+				this.write("|"); // else write it
+		}
 	}
 }
 
